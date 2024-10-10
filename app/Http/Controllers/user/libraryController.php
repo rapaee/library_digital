1ad6860e74845137924e957704b1a14647855cc6
cayadeww <cdewi5323@gmail.com>
2024-10-10 14:56:19 +0700

    update

--- app/Http/Controllers/user/libraryController.php
+++ app/Http/Controllers/user/libraryController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\buku;
 use Illuminate\Http\Request;
 
 class libraryController extends Controller
 {
     public function dashboardLibrary(){
-        return view('role.user.library');
+        $new_product = buku::with('genre')->get(); // Pastikan model dan relasi benar
+        return view('role.user.library ', compact('new_product'));
     }
 }
